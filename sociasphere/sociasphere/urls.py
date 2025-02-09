4cee286aacb443d7efe609c043f523968b8d5e76
Kakarlamudi Venkata Dhanush <kvdhanush06@hotmail.com>
2025-02-09 18:35:43 +0530

    Update urls.py

--- sociasphere/sociasphere/urls.py
+++ sociasphere/sociasphere/urls.py
@@ -26,5 +26,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
-    path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
