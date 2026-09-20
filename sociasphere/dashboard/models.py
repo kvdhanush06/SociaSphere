3458a70ffc20172452fac10ab32d061a65799983
Venkata Dhanush Kakarlamudi <kvdhanush06@gmail.com>
2026-09-20 15:11:45 +0530

    refactor: fix model lifecycle semantics

--- sociasphere/dashboard/models.py
+++ sociasphere/dashboard/models.py
@@ -1,44 +1,35 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.db.models.signals import post_save
-
-# create post model
 
 
 class Post(models.Model):
     user = models.ForeignKey(
-        User, related_name="posts",
-        on_delete=models.DO_NOTHING
+        User,
+        related_name="posts",
+        on_delete=models.CASCADE,
     )
     body = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="post_like", blank=True)
 
-    # Keep track or count of likes
-
     def number_of_likes(self):
         return self.likes.count()
 
     def __str__(self):
-        return (
-            f"{self.user} "
-            f"({self.created_at:%Y-%m-%d %H:%M}): "
-            f"{self.body}..."
-        )
+        return f"{self.user} ({self.created_at:%Y-%m-%d %H:%M}): {self.body}..."
 
 
-# Create A User Profile Model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    follows = models.ManyToManyField("self",
-                                     related_name="followed_by",
-                                     symmetrical=False,
-                                     blank=True)
-
-    date_modified = models.DateTimeField(User, auto_now=True)
-    profile_image = models.ImageField(
-        null=True, blank=True, upload_to="images/")
-
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True,
+    )
+    date_modified = models.DateTimeField(auto_now=True)
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
     profile_bio = models.CharField(null=True, blank=True, max_length=500)
     homepage_link = models.CharField(null=True, blank=True, max_length=100)
     facebook_link = models.CharField(null=True, blank=True, max_length=100)
@@ -48,17 +39,11 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-# Create Profile When New User Signs Up
-# @receiver(post_save, sender=User)
-
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-        # Have the user follow themselves
-        user_profile.follows.set([instance.profile.id])
-        user_profile.save()
+        user_profile = Profile.objects.create(user=instance)
+        user_profile.follows.add(user_profile)
 
 
 post_save.connect(create_profile, sender=User)
